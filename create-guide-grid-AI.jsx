739954489deccf53ace9838749d6949f7de9f22d
Christy Lewis <cmsering@gmail.com>
2014-05-14 14:12:07 -0500

    Initial commit

---
+++ create-guide-grid-AI.jsx
@@ -0,0 +1,43 @@
+﻿/*
+    Create Guide Grid Script for Illustrator
+    Use this script to easily create a grid of guides to use when creating CSS sprites or other scenarios where a grid would be useful for alignment.
+    
+    This is the Illustrator equivalent of sprite-grid.jsx by Filip Van Tendeloo.
+    Created by Christy Lewis.
+    
+    Tested in Illustrator CS5.1. Should work in most relatively recent AI versions.
+    
+    Usage:
+        - Download script and place into Program Files \ Adobe \ Adobe Illustrator CSx.x \ Presets \ en_US \ Scripts
+        - Restart Illustrator.
+        - Choose File -> Scripts -> create-guide-grid-AI
+*/
+
+// check for document
+if(app.documents.length > 0){  
+	// get active document & layer
+	var activeDoc = app.activeDocument;
+    var activeLayer = activeDoc.layers[0];
+	
+	// get user input on column count
+	var colWidth = parseInt( prompt("Column size?", 16) );  
+	 
+	var docWidth = activeDoc.width;
+    var docHeight = activeDoc.height;
+	
+	// determine number of columns & rows
+	var colCount = docWidth / colWidth; 
+	var rowCount = docHeight / colWidth; 
+	
+	// create vertical guidelines
+	for(i = 0; i <= colCount; ++i){        
+        var guide = activeLayer.pathItems.rectangle( 0, i * colWidth, 0, docHeight ); 
+        guide.guides = true;
+	} 	
+	
+	// create horizontal guidelines
+	for(i = 0; i <= rowCount; ++i){  
+        var guide = activeLayer.pathItems.rectangle(-i * colWidth, 0, docWidth, 0); 
+        guide.guides = true;
+	}  	
+}  
---
+++ create-guide-grid-PS.jsx
@@ -0,0 +1,40 @@
+﻿/*
+    Create Guide Grid Script for Photoshop
+    Use this script to easily create a grid of guides to use when creating CSS sprites or other scenarios where a grid would be useful for alignment.
+    
+    This is sprite-grid.jsx by Filip Van Tendeloo with a bit of code cleanup.
+    See create-guide-grid-AI.jsx for the Illustrator equivalent by Christy Lewis.
+    
+    Tested in Photoshop CS5.1. Should work in most relatively recent PS versions.
+    
+    Usage:
+        - Download script and place into Program Files \ Adobe \ Adobe Photoshop CSx.x \ Presets \ Scripts
+        - Restart Photoshop.
+        - Choose File -> Scripts -> create-guide-grid-PS
+*/
+
+// check for document
+if(app.documents.length > 0){  
+	// get active document
+	var activeDoc = app.activeDocument; 
+	
+	// get user input on column count
+	var colWidth = parseInt( prompt("Column size?", 16) );  
+	 
+	var docWidth = activeDoc.width; 
+    var docHeight = activeDoc.height;
+	
+	// determine number of columns & rows
+	var colCount = docWidth / colWidth;
+    var rowCount = docHeight / colWidth; 
+	
+	// create vertical guidelines
+	for(i = 0; i <= colCount; ++i){  
+		activeDoc.guides.add(Direction.VERTICAL, i * colWidth);  
+	}  	
+
+	// create horizontal guidelines
+	for(i = 0; i <= rowCount; ++i){  
+		activeDoc.guides.add(Direction.HORIZONTAL, i * colWidth);  
+	}  	
+}  
